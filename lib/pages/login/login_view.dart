ac58839cfdd90614d352922e81888257fba8478e
xieyezi <1435398529@qq.com>
2021-05-31 18:13:52 +0800

    feat: finish settings

--- lib/pages/login/login_view.dart
+++ lib/pages/login/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_template/router/app_pages.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -7,7 +9,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
-        onTap: () => {},
+        onTap: () => Get.toNamed(AppRoutes.Home),
         child: Container(
           child: Center(
             child: Text('登录页'),
